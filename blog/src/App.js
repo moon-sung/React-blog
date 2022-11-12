f0c60bcd0e5b3b58bed47e94b44c01bd1b157f14
moonsung <moon980708@naver.com>
2022-11-12 13:22:53 +0900

    블로그 글목록 추가

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+
+
+  let post = '맛집투어';
+  let [title, setTitle] = useState('남자 코트 추천');
+  let [title2, setTitle2] = useState('부산 고기 맛집');
+  let [title3, setTitle3] = useState('리액트 공부');
+
+  //let [total, setTotal] = useState(['첫번째', '두번째', '세번째']);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 className={post} style={{color: 'yellow', fontSize: '16px'}}>BLOG</h4>
+      </div>
+
+      <div className='list'>
+        <h4>{ title }</h4>
+        <p>today : 2022.11.10</p>
+      </div>
+
+      <div className='list'>
+        <h4>{ title2 }</h4>
+        <p>today : 2022.11.10</p>
+      </div>
+
+      <div className='list'>
+        <h4>{ title3 }</h4>
+        <p>today : 2022.11.10</p>
+      </div>
+
+      <h4>{post}</h4>
     </div>
   );
 }
